58661dddc7d7fa268c1430df8d387a15bf0dd3a6
Prakhar Dwivedi <136910948+prakhar9918@users.noreply.github.com>
2025-08-21 21:14:14 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server,{
     cors:{
-        origin: ["http://localhost:3000","https://video-call-app-flame-two.vercel.app/"]
+        origin: ["http://localhost:3000","https://video-call-app-flame-two.vercel.app/"],
         methods: ["GET", "POST"],
         credentials: true, // Allow credentials to be sent
     }
@@ -22,7 +22,8 @@ const io = new Server(server,{
 
 
 app.use(cors({
-  origin: ["http://localhost:3000","https://video-call-app-flame-two.vercel.app/"]
+  origin: ["http://localhost:3000","https://video-call-app-flame-two.vercel.app/"],
+  methods: ["GET", "POST"],
   credentials: true
 }));
 app.use(express.json());
@@ -68,4 +69,5 @@ server.listen(process.env.PORT, () => {
   console.log('Server is running on port 3000');
 
 });
+
 
